d06554d243facb6b89fde2166eb06428a7f73ced
Runia87 <ainur.sarybayeva@gmail.com>
2026-06-27 19:31:01 +0500

    Add files via upload

---
+++ exceptions/AccountIsBlockedException.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\exceptions;
+
+class AccountIsBlockedException extends \Exception
+{
+    // public function __construct($message = "Not enough money", $code = 0, Exception $previous = null) {
+    //     // make sure everything is assigned properly
+    //     parent::__construct($message, $code, $previous);
+    // }
+}
+
+
+?>
