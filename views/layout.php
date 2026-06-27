001a7c99b2f7cd779600c97d4cfcc6b6e5ccbf19
Runia87 <ainur.sarybayeva@gmail.com>
2026-06-27 19:32:37 +0500

    Add files via upload

---
+++ views/layout.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title><?=$this->e($title)?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  </head>
+  <body>
+
+
+  <nav>
+    <ul>
+        <li>
+<a href="/home">Homepage</a>
+        </li>
+        <li>
+<a href="/about">About</a>
+        </li>
+    </ul>
+
+</nav>
+    <?=$this->section('content')?>
+
+
+
+  </body>
+</html>
