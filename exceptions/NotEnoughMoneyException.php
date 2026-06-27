c050210d57f0420d7ee5abfa82e3b5fd7e8455bc
Runia87 <ainur.sarybayeva@gmail.com>
2026-06-27 19:30:14 +0500

    Add NotEnoughMoneyException class

---
+++ exceptions/NotEnoughMoneyException.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\exceptions;
+
+class NotEnoughMoneyException extends \Exception
+{
+    // public function __construct($message = "Not enough money", $code = 0, Exception $previous = null) {
+    //     // make sure everything is assigned properly
+    //     parent::__construct($message, $code, $previous);
+    // }
+}
+
+
+
+
+?>
